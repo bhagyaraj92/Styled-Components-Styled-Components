3151795d7d9306dd1503734401d2725e880f3b77
bhagyaraj92 <rajravuru92@gmail.com>
2021-08-12 08:58:07 +0000

    sytledcomponets practice

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Heading, CustomButton} from './StyledComponents'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Heading>Hello World</Heading>
+    <CustomButton type="button">Click</CustomButton>
+    <CustomButton type="button" outline>
+      Click
+    </CustomButton>
+  </>
+)
+
+export default App
